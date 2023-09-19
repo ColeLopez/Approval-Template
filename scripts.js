1c3b236ac4b2dfbbae2d3ab32ed393b0cebcf897
Cole Lopez <123988021+ColeLopez@users.noreply.github.com>
2023-09-19 13:26:05 +0200

    Item_description

--- scripts.js
+++ scripts.js
@@ -69,7 +69,7 @@ function addMore(){
     navigator.clipboard.writeText(multiText1).then(() => {
         var qty = document.getElementById("qty").value;
         var item_code = document.getElementById("item_code").value;
-        var item_description = document.getElementById("item_description").value;
+        var item_description = document.getElementById("item description").value;
 
         var sp = document.getElementById("sp").value;
         var sp_gp = document.getElementById("sp_gp").value;
