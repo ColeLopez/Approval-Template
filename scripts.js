26dccf334315c9b57766aa9332677657ba0047fe
Cole Lopez <123988021+ColeLopez@users.noreply.github.com>
2023-09-19 15:55:44 +0200

    Test no commas 4.2

--- scripts.js
+++ scripts.js
@@ -89,16 +89,18 @@ btn.addEventListener('click', function handleClick(event) {
 
 
 function submitMore(){
-    
+
     var acc_number1 = document.getElementById("acc_number").value;
     var rep_code1 = document.getElementById("rep_code").value;
     var acc_name1 = document.getElementById("acc_name").value;
     var branch_code1 = document.getElementById("branch_code").value;
 
+    var newArr = moreData.join('');
+
     var multiText2 =  'Good Day\n\n'+
     acc_number1 + ' - ' + rep_code1 + ' - ' + acc_name1 + ' - ' + branch_code1 + '\n\n' +
     'Please assist:\n' +
-    moreData.join('') +
+     newArr+
     '\nAs per ' + rep_code + '/Customer \n\n' +
     'Thank you.';
 
