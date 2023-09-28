953b00c76807956c481a15edd91e3c9433e5745b
Cole Lopez <123988021+ColeLopez@users.noreply.github.com>
2023-09-28 14:54:23 +0200

    Added in a new html file, nav-links, js to set nav-links as active, changes css to nav-liks.active

--- scripts.js
+++ scripts.js
@@ -118,3 +118,9 @@ function submitMore(){
     });
 }
 
+document.querySelectorAll(".nav-link").forEach((link) => {
+  if (link.href === window.location.href) {
+      link.classList.add("active");
+      link.setAttribute("aria-current", "page");
+  }
+});
